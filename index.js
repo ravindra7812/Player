63a6ea9393b11dc5fa4d55f03363ca01b3253096
ravindra <ravindrakumarr.dev@gmail.com>
2025-04-27 15:31:17 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express'); 
+
+const request = require('request'); 
+
+const app = express(); 
+
+ 
+
+app.use((req, res, next) => { 
+
+  // CORS headers add karna 
+
+  res.header('Access-Control-Allow-Origin', '*'); 
+
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+
+  next(); 
+
+}); 
+
+ 
+
+app.get('/proxy', (req, res) => { 
+
+  const url = req.query.url; 
+
+  if (!url) { 
+
+    return res.status(400).send('Missing url parameter'); 
+
+  } 
+
+   
+
+  // Original URL se stream fetch karke response me bhejna 
+
+  request 
+
+    .get(url) 
+
+    .on('error', err => { 
+
+      res.status(500).send('Error fetching the URL'); 
+
+    }) 
+
+    .pipe(res); 
+
+}); 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+  console.log(`running ${PORT}`);
+  
+});
+
+ 
